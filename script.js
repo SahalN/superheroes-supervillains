f521191625daa29700c85283c5de2faf1694e44b
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-09-30 10:27:40 +0700

    Add changes

---
+++ script.js
@@ -0,0 +1,10 @@
+/** @format */
+// for superheroes
+const superheroes = require("superheroes");
+const mySuperHeroes = superheroes.random();
+console.log(mySuperHeroes);
+
+//  for suppervillains
+const supervillains = require("supervillains");
+const mySuperVillains = supervillains.random();
+console.log(mySuperVillains);
